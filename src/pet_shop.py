fac94c2621f899d18e83398cf57ff7f7f4cf6dd7
Duncan Ryan <duncancryan1@gmail.com>
2020-07-17 22:00:18 +0100

    increase_pets_sold defined and tested (I will be specifiying tested as well from here on in)

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -10,3 +10,6 @@ def add_or_remove_cash(pet_shop, cash):
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] = pet_shop["admin"]["pets_sold"] + pets_sold
