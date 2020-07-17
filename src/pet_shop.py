6e17a9f8b71500a41f4e7e0550ea88b35d3749b1
Duncan Ryan <duncancryan1@gmail.com>
2020-07-17 21:49:40 +0100

    get_pets_sold defined

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -7,3 +7,6 @@ def get_total_cash(pet_shop):
 
 def add_or_remove_cash(pet_shop, cash):
     pet_shop["admin"]["total_cash"] = pet_shop["admin"]["total_cash"] + cash
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
