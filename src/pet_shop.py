3f450ec00259bd0a75152e815a59c29e4f4ed68a
Duncan Ryan <duncancryan1@gmail.com>
2020-07-17 22:38:58 +0100

    get_pets_by_breed defined and first test passed (played around with syntax until I realised referencing was incorrect - lesson learnt)

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -16,3 +16,10 @@ def increase_pets_sold(pet_shop, pets_sold):
 
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets = []
+    for pet in pet_shop["pets"]:
+        if breed == pet["breed"]:
+            pets.append(pet)
+    return pets
